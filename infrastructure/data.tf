59501e39d762730accdc6f314f4eb5b2803c8a02
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2021-01-22 15:31:26 +0000

    Initial commit

---
+++ infrastructure/data.tf
@@ -0,0 +1,8 @@
+data "azurerm_client_config" "current"{
+}
+
+data "azurerm_key_vault" "core-kv"{
+    name = var.keyvault_name
+    resource_group_name = var.keyvault_rg
+}
+
---
+++ infrastructure/variables.tf
@@ -0,0 +1,14 @@
+variable "env" { 
+    description = "Name of the environment to deploy infrastructure"
+    type = string
+}
+
+variable "product" {
+    description = "Name of the product"
+    type = string   
+}
+
+variable "location" {
+    description = "Azure location used for resource deployment"
+    type = string
+}
