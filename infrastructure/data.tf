d1fc1701137837d4985dc26e43293d1c69b97f93
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2021-01-25 16:08:03 +0000

    fmt issue fixed

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,8 +1,8 @@
-data "azurerm_client_config" "current"{
+data "azurerm_client_config" "current" {
 }
 
-data "azurerm_key_vault" "core-kv"{
-    name = var.keyvault_name
-    resource_group_name = var.keyvault_rg
+data "azurerm_key_vault" "core-kv" {
+  name                = var.keyvault_name
+  resource_group_name = var.keyvault_rg
 }
 
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,14 +1,14 @@
-variable "env" { 
-    description = "Name of the environment to deploy infrastructure"
-    type = string
+variable "env" {
+  description = "Name of the environment to deploy infrastructure"
+  type        = string
 }
 
 variable "product" {
-    description = "Name of the product"
-    type = string   
+  description = "Name of the product"
+  type        = string
 }
 
 variable "location" {
-    description = "Azure location used for resource deployment"
-    type = string
+  description = "Azure location used for resource deployment"
+  type        = string
 }
