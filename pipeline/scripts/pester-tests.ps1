aa67abee4f39c0f4f63f540efe54276675653f49
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2021-01-25 15:28:29 +0000

    update pipeline

--- pipeline/scripts/pester-tests.ps1
+++ pipeline/scripts/pester-tests.ps1
@@ -1,2 +1,2 @@
 Install-Module -Name Pester -MaximumVersion 4.99.99 -Force -Verbose -Scope CurrentUser
-Invoke-Pester ./pipeline/scripts/ci -OutputFormat NUnitXml -OutputFile ./TEST-CI.xml -EnableExit
+Invoke-Pester ./ci -OutputFormat NUnitXml -OutputFile ./TEST-CI.xml -EnableExit
