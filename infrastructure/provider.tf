bba97367fb1542e897608e8fe2cc10b07457be72
mokainos <72137340+mokainos@users.noreply.github.com>
2021-01-27 17:07:32 +0000

    added backend

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.14.5"
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.44.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
