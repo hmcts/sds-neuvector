05e99354af4c49cbaed89d3ae1750833900bc79e
mokainos <72137340+mokainos@users.noreply.github.com>
2021-02-04 15:26:46 +0000

    Update infrastructure/storageaccount.tf
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>

--- infrastructure/storageaccount.tf
+++ infrastructure/storageaccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "sdsneuvector${var.environment}-rg"
+  name     = "sds-neuvector-${var.environment}-rg"
   location = var.location
   tags     = local.common_tags
 }
