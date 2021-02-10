618c72ba10a332600244e0dd36a963c96f87920b
mokainos <72137340+mokainos@users.noreply.github.com>
2021-02-10 11:54:08 +0000

    update variable

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -6,5 +6,5 @@ module "azurekeyvault" {
   managed_identity_object_ids = [azurerm_user_assigned_identity.managed_identity.principal_id]
   resource_group_name         = azurerm_resource_group.rg.name
   product_group_name          = "DTS Platform Operations"
-  common_tags                 = var.common_tags
+  common_tags                 = local.common_tags
 }
