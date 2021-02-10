c7dfb5486f66464a2fd105fbb387aefd79508b15
mokainos <72137340+mokainos@users.noreply.github.com>
2021-02-10 11:50:27 +0000

    deleted MI

--- infrastructure/identity.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_user_assigned_identity" "managed_identity" {
-  resource_group_name = "managed-identities-${var.environment}-rg"
-  location            = var.location
-
-  name = "${var.product}-${var.environment}-mi"
-
-  tags = local.common_tags
-}
