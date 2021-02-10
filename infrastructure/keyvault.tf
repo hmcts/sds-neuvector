1cfa474b4794805e5ec072c56c2dab7680aeb292
mokainos <72137340+mokainos@users.noreply.github.com>
2021-02-10 10:59:34 +0000

    Post update of KV  module

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -1,10 +1,10 @@
 module "azurekeyvault" {
-  source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
-  name                    = "sds${var.product}-${var.environment}"
-  product                 = "${var.product}"
-  env                     = "${var.environment}"
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = azurerm_user_assigned_identity.managed_identity.principal_id
-  resource_group_name     = azurerm_resource_group.rg.name
-  product_group_object_id = "253a03f0-6310-40d2-a286-1de6ac85f052" //DTS platform operations team 
+  source                      = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
+  name                        = "sds${var.product}-${var.environment}"
+  product                     = "${var.product}"
+  env                         = "${var.environment}"
+  managed_identity_object_ids = [azurerm_user_assigned_identity.managed_identity.principal_id]
+  resource_group_name         = azurerm_resource_group.rg.name
+  product_group_name          = "DTS Platform Operations"
+  common_tags                 = var.common_tags
 }
