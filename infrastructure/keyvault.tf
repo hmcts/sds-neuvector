16e0fecb7d4c6af60334d7771e60460ab567d886
mokainos <72137340+mokainos@users.noreply.github.com>
2021-02-10 12:09:53 +0000

    update variable

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -1,10 +1,11 @@
 module "azurekeyvault" {
-  source                      = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
-  name                        = "sds${var.product}-${var.environment}"
-  product                     = var.product
-  env                         = var.environment
-  managed_identity_object_ids = [azurerm_user_assigned_identity.managed_identity.principal_id]
-  resource_group_name         = azurerm_resource_group.rg.name
-  product_group_name          = "DTS Platform Operations"
-  common_tags                 = local.common_tags
+  source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
+  name                    = "sds${var.product}-${var.environment}"
+  product                 = var.product
+  env                     = var.environment
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_name      = "DTS Platform Operations"
+  common_tags             = local.common_tags
+  create_managed_identity = true
+
 }
