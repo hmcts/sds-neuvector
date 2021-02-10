bd8041976ea9e4fe608ff1a94f8d932c559a2217
mokainos <72137340+mokainos@users.noreply.github.com>
2021-02-10 10:00:42 +0000

    using kv module

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -1,29 +1,10 @@
-resource "azurerm_key_vault" "kv" {
-  name                       = "sds${var.product}-${var.environment}"
-  location                   = var.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days = 7
-  purge_protection_enabled   = true
-  tags                       = local.common_tags
-  sku_name                   = "standard"
-
-  access_policy {
-    object_id = azurerm_user_assigned_identity.managed_identity.principal_id
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    key_permissions = [
-      "get",
-      "list",
-    ]
-
-    secret_permissions = [
-      "get",
-      "list",
-    ]
-
-    storage_permissions = [
-      "get",
-      "list",
-    ]
-  }
+module "azurekeyvault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                    = "sds${var.product}-${var.environment}"
+  product                 = "${var.product}"
+  env                     = "${var.environment}"
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = azurerm_user_assigned_identity.managed_identity.principal_id
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_object_id = "253a03f0-6310-40d2-a286-1de6ac85f052" //DTS platform operations team 
 }
