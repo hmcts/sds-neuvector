d5ab1b0a042f5c53bb62df0f123ab9a0233b4366
Enda Kelly <enda.kelly@hmcts.net>
2022-07-05 14:05:11 +0100

    use common tags

--- infrastructure/keyvault.tf
+++ infrastructure/keyvault.tf
@@ -5,7 +5,7 @@ module "azurekeyvault" {
   env                     = var.environment
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_name      = "DTS Platform Operations"
-  common_tags             = local.common_tags
+  common_tags             = module.ctags.common_tags
   create_managed_identity = true
   object_id               = data.azurerm_client_config.current.object_id
 }
