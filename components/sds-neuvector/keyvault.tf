80a2cc3b974fe6a613ebf6ef1d54b73f787b4e34
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-08-10 11:57:18 +0100

    Update keyvault.tf

--- components/sds-neuvector/keyvault.tf
+++ components/sds-neuvector/keyvault.tf
@@ -1,14 +1,13 @@
 module "azurekeyvault" {
-  source                     = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
-  name                       = "cft${var.product}-${var.env}"
-  product                    = var.product
-  env                        = var.env
-  resource_group_name        = azurerm_resource_group.rg.name
-  product_group_name         = "DTS Platform Operations"
-  common_tags                = module.ctags.common_tags
-  object_id                  = data.azurerm_client_config.current.object_id
-  managed_identity_object_id = data.azurerm_user_assigned_identity.managed_identity.principal_id
-  create_managed_identity    = true
+  source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
+  name                    = "sds${var.product}-${var.environment}"
+  product                 = var.product
+  env                     = var.environment
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_name      = "DTS Platform Operations"
+  common_tags             = module.ctags.common_tags
+  create_managed_identity = true
+  object_id               = data.azurerm_client_config.current.object_id
 }
 
 resource "azurerm_key_vault_access_policy" "sc_access_policy" {
