cd3ee3c775a8332c1fc65028003459390b829660
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-08-10 12:13:02 +0100

    Update storageaccount.tf

--- components/sds-neuvector/storageaccount.tf
+++ components/sds-neuvector/storageaccount.tf
@@ -1,6 +1,6 @@
 module "ctags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = var.environment
+  environment = var.env
   product     = var.product
   builtFrom   = var.builtFrom
 }
