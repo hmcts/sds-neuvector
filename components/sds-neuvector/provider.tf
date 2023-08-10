680b818d0e86c4f80037182dcddf02129dbf3203
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-08-10 10:02:33 +0100

    Create provider.tf

---
+++ components/sds-neuvector/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.5.0"
+  backend "azurerm" {
+    subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.42.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
