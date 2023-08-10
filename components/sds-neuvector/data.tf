1104afe2caab6af71c85e53f6d04ad35652205f0
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-08-10 11:57:45 +0100

    Update data.tf

--- components/sds-neuvector/data.tf
+++ components/sds-neuvector/data.tf
@@ -1,12 +1,2 @@
 data "azurerm_client_config" "current" {
 }
-
-data "azurerm_user_assigned_identity" "managed_identity" {
-  name                = "${var.product}-${var.env}-mi"
-  resource_group_name = "managed-identities-${var.env}-rg"
-}
-
-data "azuread_group" "platops_sc" {
-  display_name     = "DTS Platform Operations SC"
-  security_enabled = true
-}
