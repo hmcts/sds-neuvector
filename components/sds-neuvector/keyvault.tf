6b85adfdafea6256cfae25667668125b5df85288
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2023-08-10 12:13:13 +0100

    Update keyvault.tf

--- components/sds-neuvector/keyvault.tf
+++ components/sds-neuvector/keyvault.tf
@@ -2,7 +2,7 @@ module "azurekeyvault" {
   source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
   name                    = "sds${var.product}-${var.environment}"
   product                 = var.product
-  env                     = var.environment
+  env                     = var.env
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_name      = "DTS Platform Operations"
   common_tags             = module.ctags.common_tags
