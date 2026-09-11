e4abcbd496e5337140e19ec0bac5e536d71131c0
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-11 15:54:03 +0100

    DTSPO-34656: update key vault module branch

--- components/sds-neuvector/keyvault.tf
+++ components/sds-neuvector/keyvault.tf
@@ -1,5 +1,5 @@
 module "azurekeyvault" {
-  source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=master"
+  source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
   name                    = "sds${var.product}-${var.env}"
   product                 = var.product
   env                     = var.env
